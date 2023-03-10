a916c728ce7babb2f70d9311ec49ab499c7e7b73
Ubuntu <ubuntu@ip-172-31-30-20.ec2.internal>
2023-03-10 19:06:34 +0000

    customize devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  devise_for :users
+  devise_for :users, :controllers => { 
+    registrations: 'users/registrations' 
+  }
   root to: 'welcome#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
